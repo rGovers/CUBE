a2465a0e1eef6cf668f3ab069bb7d55ea38e173a
rGovers <govers.river.r@gmail.com>
2023-09-20 17:22:21 +1000

    Added license to files

--- include/CUBE/CUBECommandLine.h
+++ include/CUBE/CUBECommandLine.h
@@ -1,3 +1,7 @@
+// CUBE - Header only C/C++/C# build system.
+// 
+// License at end of file.
+
 #ifndef INCLUDED_HEADER_CUBECOMMANDLINE
 #define INCLUDED_HEADER_CUBECOMMANDLINE
 
@@ -336,3 +340,25 @@ void CUBE_CommandLine_Destroy(CUBE_CommandLine* a_commandLine)
 #endif
 
 #endif
+
+// MIT License
+// 
+// Copyright (c) 2023 River Govers
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
--- include/CUBE/CUBEPath.h
+++ include/CUBE/CUBEPath.h
@@ -1,3 +1,7 @@
+// CUBE - Header only C/C++/C# build system.
+// 
+// License at end of file.
+
 #ifndef INCLUDED_HEADER_CUBEPATH
 #define INCLUDED_HEADER_CUBEPATH
 
@@ -274,3 +278,25 @@ CUBE_String CUBE_Path_Filename(const CUBE_Path* a_path)
 #endif
 
 #endif
+
+// MIT License
+// 
+// Copyright (c) 2023 River Govers
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
